7f4f26dbcfd7206b1d85c372b1c3af7ec01a1a9c
Joshua Alexander <83144672+joshuaalexanderio@users.noreply.github.com>
2025-08-29 05:03:46 -0600

    chore: remove unused import and redundant @Mapping annotations

--- src/main/java/dev/joshalexander/recipeappbackend/mapper/RecipeMapper.java
+++ src/main/java/dev/joshalexander/recipeappbackend/mapper/RecipeMapper.java
@@ -7,7 +7,6 @@ import dev.joshalexander.recipeappbackend.entity.Ingredient;
 import dev.joshalexander.recipeappbackend.entity.Recipe;
 import dev.joshalexander.recipeappbackend.entity.User;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
@@ -16,10 +15,6 @@ public interface RecipeMapper {
     RecipeMapper INSTANCE = Mappers.getMapper(RecipeMapper.class);
 
     IngredientDTO toIngredientDTO(Ingredient ingredient);
-
     UserDTO toUserDTO(User user);
-
-    @Mapping(target = "user", source = "user")
-    @Mapping(target = "ingredients", source = "ingredients")
     RecipeDTO toRecipeDTO(Recipe recipe);
 }
