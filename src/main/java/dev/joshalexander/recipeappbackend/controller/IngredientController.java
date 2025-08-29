bb2f6a7baddc7f4dbc739ecfcf770edd480ac137
Joshua Alexander <83144672+joshuaalexanderio@users.noreply.github.com>
2025-08-29 05:24:47 -0600

    feat: implement /users and /ingredients endpoints

--- src/main/java/dev/joshalexander/recipeappbackend/controller/IngredientController.java
+++ src/main/java/dev/joshalexander/recipeappbackend/controller/IngredientController.java
@@ -1,4 +1,30 @@
 package dev.joshalexander.recipeappbackend.controller;
+import dev.joshalexander.recipeappbackend.dto.IngredientDTO;
+import dev.joshalexander.recipeappbackend.dto.UserDTO;
+import dev.joshalexander.recipeappbackend.mapper.EntityMapper;
+import dev.joshalexander.recipeappbackend.repository.IngredientRepository;
+import dev.joshalexander.recipeappbackend.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ingredients")
 public class IngredientController {
+    private final EntityMapper ingredientMapper;
+    private final IngredientRepository ingredientRepository;
+
+    public IngredientController(EntityMapper ingredientMapper, IngredientRepository ingredientRepository) {
+        this.ingredientMapper = ingredientMapper;
+        this.ingredientRepository = ingredientRepository;
+    }
+    @GetMapping
+    public List<IngredientDTO> getAllUsers() {
+        return ingredientRepository.findAll()
+                .stream()
+                .map(ingredientMapper::toIngredientDTO)
+                .toList();
+    }
 }
--- src/main/java/dev/joshalexander/recipeappbackend/controller/UserController.java
+++ src/main/java/dev/joshalexander/recipeappbackend/controller/UserController.java
@@ -1,4 +1,28 @@
 package dev.joshalexander.recipeappbackend.controller;
+import dev.joshalexander.recipeappbackend.dto.UserDTO;
+import dev.joshalexander.recipeappbackend.mapper.EntityMapper;
+import dev.joshalexander.recipeappbackend.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+    private final EntityMapper userMapper;
+    private final UserRepository userRepository;
+
+    public UserController(EntityMapper userMapper, UserRepository userRepository) {
+        this.userMapper = userMapper;
+        this.userRepository = userRepository;
+    }
+    @GetMapping
+    public List<UserDTO> getAllUsers() {
+        return userRepository.findAll()
+                .stream()
+                .map(userMapper::toUserDTO)
+                .toList();
+    }
 }
