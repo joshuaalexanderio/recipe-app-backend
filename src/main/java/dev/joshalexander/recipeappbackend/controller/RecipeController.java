bb7324cfa711ff1b0235b7cbc4fee997289f1adb
Joshua Alexander <83144672+joshuaalexanderio@users.noreply.github.com>
2025-09-03 23:04:40 -0600

    refactor: update RecipeController to use service layer

--- src/main/java/dev/joshalexander/recipeappbackend/controller/RecipeController.java
+++ src/main/java/dev/joshalexander/recipeappbackend/controller/RecipeController.java
@@ -1,33 +1,33 @@
 package dev.joshalexander.recipeappbackend.controller;
-
 import dev.joshalexander.recipeappbackend.dto.RecipeDTO;
-import dev.joshalexander.recipeappbackend.mapper.EntityMapper;
-import dev.joshalexander.recipeappbackend.repository.RecipeRepository;
+import dev.joshalexander.recipeappbackend.service.RecipeService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/recipes")
 public class RecipeController {
-    private final EntityMapper recipeMapper;
-
-    private final RecipeRepository recipeRepository;
-
-    public RecipeController(RecipeRepository recipeRepository, EntityMapper recipeMapper) {
-        this.recipeRepository = recipeRepository;
-        this.recipeMapper = recipeMapper;
+    private final RecipeService recipeService;
+    public RecipeController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @GetMapping
-    public List<RecipeDTO> getAllRecipes() {
-        return recipeRepository.findAll()
-                .stream()
-                .map(recipeMapper::toRecipeDTO)
-                .toList();
+    public ResponseEntity<List<RecipeDTO>> getAllRecipes() {
+        List<RecipeDTO> users = recipeService.getAllRecipes();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{recipeId}")
+    public ResponseEntity<RecipeDTO> getRecipeById(@PathVariable Long recipeId) {
+        Optional<RecipeDTO> user = recipeService.getRecipeById(recipeId);
+
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
-
-
