3573737fd071d671c4f10c0868fa5e9e6424f5f1
Joshua Alexander <83144672+joshuaalexanderio@users.noreply.github.com>
2025-10-07 14:24:31 -0600

    chore: remove unused imports

--- src/main/java/dev/joshalexander/recipeappbackend/repository/RecipeIngredientRepository.java
+++ src/main/java/dev/joshalexander/recipeappbackend/repository/RecipeIngredientRepository.java
@@ -4,11 +4,9 @@ import dev.joshalexander.recipeappbackend.entity.RecipeIngredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 
 @Repository
