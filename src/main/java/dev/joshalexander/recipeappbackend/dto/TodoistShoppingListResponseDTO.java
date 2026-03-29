9ceb95abdf9e52943a3b1fb55e39e1dd2db3a641
joshuaalexanderio <joshuaalexandertb@gmail.com>
2026-03-29 14:34:10 -0600

    feat: use provided projectId for Todoist shopping list, fall back to inbox

--- src/main/java/dev/joshalexander/recipeappbackend/dto/TodoistShoppingListResponseDTO.java
+++ src/main/java/dev/joshalexander/recipeappbackend/dto/TodoistShoppingListResponseDTO.java
@@ -7,7 +7,6 @@ import java.util.List;
  */
 public record TodoistShoppingListResponseDTO(
         String projectId,
-        String projectName,
         int taskCount,
         List<String> taskIds
 ) {
