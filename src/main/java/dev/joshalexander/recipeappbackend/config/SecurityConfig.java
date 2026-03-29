e35a63544f1932a44e83b93bc59e7151da9d305e
joshuaalexanderio <joshuaalexandertb@gmail.com>
2026-03-29 01:50:11 -0600

    feat(security-config): add my joshalexander.dev to allowed origins

--- src/main/java/dev/joshalexander/recipeappbackend/config/SecurityConfig.java
+++ src/main/java/dev/joshalexander/recipeappbackend/config/SecurityConfig.java
@@ -52,7 +52,11 @@ public class SecurityConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
-    config.setAllowedOrigins(List.of("http://localhost:4200"));
+    config.setAllowedOrigins(List.of(
+        "http://localhost:4200",
+        "https://joshalexander.dev",
+        "https://www.joshalexander.dev"
+    ));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
     config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
     config.setAllowCredentials(true); // required for Basic auth headers to be sent
